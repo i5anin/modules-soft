db21f2a4bbb9d05ac47765f0cfe84f3a689379da
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-09-03 16:57:14 +0300

    +

--- client/src/components/Table.vue
+++ client/src/components/Table.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <table class="table table-striped">
+    <table id="ordersTable" class="table table-striped">
       <thead>
       <tr>
         <th></th>
@@ -17,112 +17,98 @@
       </tr>
       </thead>
       <tbody>
-      <tr v-for="order in orders" :key="order.id">
-        <td>
-          <input type="checkbox" :checked="order.status_ready" disabled>
-        </td>
-        <td :class="{ 'table-danger': order.status_cal }" class="text-center">
-          {{ order.status_cal }}
-        </td>
-        <td :class="{ 'table-warning': order.status_instr }" class="text-center">
-          {{ order.status_instr }}
-        </td>
-        <td :class="{ 'table-secondary': order.status_draft }" class="text-center">
-          {{ order.status_draft }}
-        </td>
-        <td :class="{ 'table-dark': order.status_metall }" class="text-center">
-          {{ order.status_metall }}
-        </td>
-        <td :class="{ 'table-success': order.status_kp }" class="text-center">
-          {{ order.status_kp }}
-        </td>
-        <td>{{ order.id }}</td>
-        <td>{{ formatDate(order.date) }}</td>
-        <td :class="{ 'table-success': order.goz }">
-          {{ order.name }}
-        </td>
-        <td>{{ order.cal_buy_time }}</td>
-        <td>{{ order.order_manager }}</td>
-      </tr>
       </tbody>
     </table>
-
-    <nav aria-label="Page navigation">
-      <ul class="pagination">
-        <li class="page-item" :class="{ disabled: currentPage === 1 }">
-          <button class="page-link" @click="prevPage">Предыдущая</button>
-        </li>
-        <li class="page-item active" aria-current="page">
-          <span class="page-link">{{ currentPage }}</span>
-        </li>
-        <li class="page-item" :class="{ disabled: currentPage === totalPages }">
-          <button class="page-link" @click="nextPage">Следующая</button>
-        </li>
-      </ul>
-    </nav>
   </div>
 </template>
 
 <script>
-import { computed, onMounted, ref, watch } from 'vue';
+import DataTable from 'datatables.net-dt';
+import { onMounted, ref, watch } from 'vue';
 import { useHomeStore } from '../store/home.module';
 import { getOrders } from '../api/orders';
-import { format } from 'date-fns'; // Импортируем format из date-fns
-import { ru } from 'date-fns/locale/ru'; // Импортируем русскую локаль
+import { format } from 'date-fns';
+import { ru } from 'date-fns/locale/ru';
+
+import 'datatables.net-bs5';
 
 export default {
   setup() {
     const homeStore = useHomeStore();
-    const orders = ref([]); // Реактивный массив для хранения заказов
-    const totalOrders = ref(0); // Реактивная переменная для общего количества заказов
+    const ordersTable = ref(null); // Реактивная переменная для таблицы DataTables
 
-    // Вычисляемые свойства для текущей страницы и общего количества страниц
-    const currentPage = computed(() => homeStore.currentPage);
-    const totalPages = computed(() => Math.ceil(totalOrders.value / homeStore.itemsPerPage));
-
-    // Функция для загрузки заказов
-    const fetchOrders = async () => {
-      try {
-        const data = await getOrders(homeStore.currentPage, homeStore.itemsPerPage, homeStore.searchQuery);
-        orders.value = data.orders;
-        totalOrders.value = data.totalCount;
-      } catch (error) {
-        console.error('Ошибка при загрузке заказов:', error);
-      }
+    // Функция для инициализации DataTables
+    const initDataTable = () => {
+      ordersTable.value = new DataTable('#ordersTable', {
+        processing: true, // Включаем индикатор загрузки
+        serverSide: true, // Включаем серверную обработку
+        ajax: (data, callback, settings) => {
+          // Функция для обработки AJAX-запроса DataTables
+          const page = Math.floor(data.start / data.length) + 1;
+          getOrders(page, data.length, homeStore.searchQuery)
+              .then(response => {
+                callback({
+                  data: response.orders,
+                  recordsTotal: response.totalCount,
+                  recordsFiltered: response.totalCount,
+                });
+              })
+              .catch(error => {
+                console.error('Ошибка при загрузке заказов:', error);
+              });
+        },
+        columns: [
+          {
+            data: 'status_ready', orderable: false, className: 'text-center',
+            render: data => `<input type="checkbox" ${data ? 'checked' : ''} disabled>`
+          },
+          {
+            data: 'status_cal', className: 'text-center',
+            render: (data, type, row) => `<td class="${data ? 'table-danger' : ''} text-center">${data}</td>`
+          },
+          {
+            data: 'status_instr', className: 'text-center',
+            render: (data, type, row) => `<td class="${data ? 'table-warning' : ''} text-center">${data}</td>`
+          },
+          {
+            data: 'status_draft', className: 'text-center',
+            render: (data, type, row) => `<td class="${data ? 'table-secondary' : ''} text-center">${data}</td>`
+          },
+          {
+            data: 'status_metall', className: 'text-center',
+            render: (data, type, row) => `<td class="${data ? 'table-dark' : ''} text-center">${data}</td>`
+          },
+          {
+            data: 'status_kp', className: 'text-center',
+            render: (data, type, row) => `<td class="${data ? 'table-success' : ''} text-center">${data}</td>`
+          },
+          {data: 'id'},
+          {data: 'date', render: data => format(new Date(data), 'dd.MM.yyyy', {locale: ru})},
+          {
+            data: 'name',
+            render: (data, type, row) => `<td class="${row.goz ? 'table-success' : ''}">${data}</td>`
+          },
+          {data: 'cal_buy_time'},
+          {data: 'order_manager'},
+        ],
+        language: { // Настройки языка для DataTables (опционально)
+          url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Russian.json'
+        }
+      });
     };
 
-    // Функция для форматирования даты с помощью date-fns
-    const formatDate = (dateString) => {
-      const date = new Date(dateString);
-      return format(date, 'dd.MM.yyyy', { locale: ru }); // Форматируем в "ДД.ММ.ГГГГ"
-    };
+    // Следим за изменением searchQuery для обновления таблицы
+    watch(() => homeStore.searchQuery, () => {
+      ordersTable.value.ajax.reload();
+    });
 
-    // Следим за изменениями currentPage и searchQuery для загрузки заказов
-    watch([currentPage, () => homeStore.searchQuery], fetchOrders);
-
-    // Функции для перехода на следующую и предыдущую страницы
-    const nextPage = () => {
-      if (homeStore.currentPage < totalPages.value) {
-        homeStore.nextPage();
-      }
-    };
-
-    const prevPage = () => {
-      if (homeStore.currentPage > 1) {
-        homeStore.prevPage();
-      }
-    };
-
-    // Загружаем заказы при монтировании компонента
-    onMounted(fetchOrders);
+    onMounted(() => {
+      // Инициализируем DataTables после монтирования компонента
+      initDataTable();
+    });
 
     return {
-      orders,
-      currentPage,
-      totalPages,
-      nextPage,
-      prevPage,
-      formatDate // Добавляем formatDate в возвращаемый объект
+      ordersTable,
     };
   },
 };
