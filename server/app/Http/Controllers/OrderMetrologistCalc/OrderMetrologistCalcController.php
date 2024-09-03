a41bd6905e44a0477b5c3380ceb302c42d58ee70
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-09-03 09:30:33 +0300

    +

--- server/app/Http/Controllers/OrderMetrologistCalc/OrderMetrologistCalcController.php
+++ server/app/Http/Controllers/OrderMetrologistCalc/OrderMetrologistCalcController.php
@@ -6,54 +6,40 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Pagination\LengthAwarePaginator;
-
 
 class OrderMetrologistCalcController extends Controller
 {
     public function getOrdersData(Request $request): JsonResponse
     {
         try {
-            $params = $request->query(); // Используем $request->query() для параметров GET
-            $search = $params['search'] ?? null;
-            $page = (int)($params['page'] ?? 1);
-            $limit = (int)($params['limit'] ?? 15);
+            $search = $request->query('search');
+            $page = (int)($request->query('page') ?? 1);
+            $limit = (int)($request->query('limit') ?? 15);
             $offset = ($page - 1) * $limit;
 
-            $sql = file_get_contents(database_path('sql/ordersList.sql'));
-            $bindings = [];
+            $sql = file_get_contents(database_path('sql\getOrdersData.sql'));
 
-            // Search Filtering
             if ($search) {
-                $sql .= " AND (orders.id::text ILIKE ? OR clients.name ILIKE ?)";
-                $bindings[] = "%$search%";
-                $bindings[] = "%$search%";
+                $searchSQL = "AND (orders.id::text ILIKE :search OR clients.name ILIKE :search)";
+                $sql = str_replace('-- SEARCH_CONDITION', $searchSQL, $sql);
+            } else {
+                $sql = str_replace('-- SEARCH_CONDITION', '', $sql);
             }
 
-            // Count total results before applying LIMIT/OFFSET
-            $totalResultsSql = preg_replace('/LIMIT\s+\d+\s+OFFSET\s+\d+/i', '', $sql);
-            $totalResults = count(DB::select(DB::raw($totalResultsSql), $bindings));
+            $parameters = [
+                'limit' => $limit,
+                'offset' => $offset
+            ];
 
-            // Apply LIMIT/OFFSET for pagination
-            $sql .= " LIMIT {$limit} OFFSET {$offset}";
+            if ($search) {
+                $parameters['search'] = "%{$search}%";
+            }
 
-            $orders = DB::select(DB::raw($sql), $bindings);
+            $orders = DB::select($sql, $parameters);
 
-            // Create Paginator
-            $paginator = new LengthAwarePaginator($orders, $totalResults, $limit, $page, [
-                'path' => $request->url(),
-                'query' => $request->query(),
-            ]);
-
-            return response()->json([
-                'currentPage' => $paginator->currentPage(),
-                'itemsPerPage' => $paginator->perPage(),
-                'totalCount' => $paginator->total(),
-                'orders' => $paginator->items()
-            ]);
-
+            return response()->json($orders);
         } catch (\Exception $e) {
-            return response()->json(['message' => 'Ошибка при получении информации о заказах'], 500);
+            return response()->json(['message' => $e->getMessage()], 500);
         }
     }
 }
--- server/database/sql/getOrdersData.sql
+++ server/database/sql/getOrdersData.sql
@@ -1,51 +1,65 @@
-SELECT orders.status_ready,
-       CASE
-           WHEN COUNT(*) FILTER (WHERE ordersnom.status_cal = 'К' AND ordersnom.kolvo > 0 AND NOT orders.status_ready) >
-                0 THEN 'К'
-           ELSE ' '
-           END                     AS status_cal,
-       CASE
-           WHEN COUNT(*) FILTER (WHERE ordersnom.status_instr = 'И' AND ordersnom.kolvo > 0) > 0 THEN 'И'
-           ELSE ' '
-           END                     AS status_instr,
-       CASE
-           WHEN COUNT(*) FILTER (WHERE ordersnom.status_draft = 'Ч' AND ordersnom.kolvo > 0) > 0 THEN 'Ч'
-           ELSE ' '
-           END                     AS status_draft,
-       CASE
-           WHEN COUNT(*) FILTER (WHERE ordersnom.status_metall = 'М' AND ordersnom.kolvo > 0) > 0 OR
-                NOT dbo.st_set(orders.id) THEN 'М'
-           ELSE ' '
-           END                     AS status_metall,
-       CASE
-           WHEN COUNT(*) FILTER (WHERE ordersnom.status_kp = 'КП' AND ordersnom.kolvo > 0 AND NOT orders.status_ready) >
-                0
-               AND COUNT(*) FILTER (WHERE ordersnom.kolvo > 0 AND
-                                          (ordersnom.status_cal = 'К' OR ordersnom.status_instr = 'И' OR
-                                           ordersnom.status_draft = 'Ч' OR ordersnom.status_metall = 'М')) = 0
-               AND dbo.st_set(orders.id) THEN 'КП'
-           ELSE ' '
-           END                     AS status_kp,
-       orders.id,
-       orders.date,
-       clients.name,
-       orders.client_id,
-       MAX(ordersnom.cal_buy_time) AS cal_buy_time,
-       orders.order_manager,
-       orders.locked,
-       orders.is_complete,
-       orders.count_pos,
-       orders.comments,
-       orders.goz,
-       tools.name
-FROM dbo.clients AS clients
-         JOIN dbo.orders AS orders ON orders.client_id = clients.id
-         JOIN dbo.ordersnom AS ordersnom ON ordersnom.order_id = orders.id
-         JOIN dbo.cal_info AS cal_info ON ordersnom.id = cal_info.ordersnom_id
-         JOIN dbo.tools ON cal_info.tool_id = tools.id -- Join with the tools table
-WHERE 1 = 1
-/* Dynamic WHERE clause will be added here */
-GROUP BY orders.order_manager, orders.status_ready, orders.id, orders.date, clients.name,
-         orders.client_id, orders.locked, orders.is_complete, orders.count_pos, orders.comments, orders.goz, tools.name
-ORDER BY orders.status_cal DESC, orders.id DESC
-/* LIMIT and OFFSET will be added here for pagination */
+SELECT
+    orders.status_ready,
+    CASE WHEN COUNT(*) FILTER (WHERE ordersnom.status_cal='К' AND NOT orders.status_ready AND ordersnom.kolvo > 0) > 0 THEN 'К' ELSE ' ' END AS status_cal,
+    CASE WHEN COUNT(*) FILTER (WHERE ordersnom.status_instr='И' AND NOT orders.status_ready AND ordersnom.kolvo > 0) > 0 THEN 'И' ELSE ' ' END AS status_instr,
+    CASE WHEN COUNT(*) FILTER (WHERE ordersnom.status_draft='Ч' AND NOT orders.status_ready AND ordersnom.kolvo > 0) > 0 THEN 'Ч' ELSE ' ' END AS status_draft,
+    CASE WHEN COUNT(*) FILTER (WHERE ordersnom.status_metall='М' AND NOT orders.status_ready AND ordersnom.kolvo > 0) > 0 OR NOT dbo.st_set(orders.id) THEN 'М' ELSE ' ' END AS status_metall,
+    CASE WHEN COUNT(*) FILTER (WHERE ordersnom.status_kp='КП' AND ordersnom.kolvo > 0 AND NOT orders.status_ready) > 0 AND
+              COUNT(*) FILTER (WHERE
+                  ordersnom.kolvo > 0 AND
+                  (ordersnom.status_cal='К' OR
+                   ordersnom.status_instr='И' OR
+                   ordersnom.status_draft='Ч' OR
+                   ordersnom.status_metall='М')
+                  ) = 0 AND dbo.st_set(orders.id) THEN 'КП' ELSE ' ' END AS status_kp,
+    orders.id,
+    orders.date,
+    clients.name,
+    orders.nds,
+    orders.comments,
+    orders.tech_fio,
+    clients.contact,
+    orders.client_id,
+    SUM(ordersnom.metall_price_total) as metall_price_total,
+    orders.logistics_price,
+    MAX(ordersnom.metall_buy_time) as metall_buy_time,
+    orders.locked,
+    orders.logistics_price_per_detail,
+    clients.phone,
+    clients.email,
+    orders.is_complete,
+    orders.count_pos,
+    orders.omts_comments,
+    orders.order_manager,
+    orders.goz
+FROM
+    dbo.orders
+        JOIN dbo.clients ON orders.client_id = clients.id
+        JOIN dbo.ordersnom ON ordersnom.order_id = orders.id
+WHERE
+    orders.deleted = false
+-- SEARCH_CONDITION
+GROUP BY
+    orders.status_ready,
+    orders.id,
+    orders.date,
+    clients.name,
+    orders.nds,
+    orders.comments,
+    orders.tech_fio,
+    clients.contact,
+    orders.client_id,
+    orders.logistics_price,
+    orders.locked,
+    orders.logistics_price_per_detail,
+    clients.phone,
+    clients.email,
+    orders.is_complete,
+    orders.count_pos,
+    orders.omts_comments,
+    orders.order_manager,
+    orders.goz
+ORDER BY
+    orders.id DESC
+LIMIT :limit
+    OFFSET :offset
