a8de7131eb69f463b5df3cee779c696edc222d0c
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-09-04 16:54:47 +0300

    Update Table.vue

--- client/src/components/Table.vue
+++ client/src/components/Table.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
+<!--    <div class="row mb-3">-->
+<!--      <div class="col-md-3">-->
+<!--        <input type="text" class="form-control" placeholder="Поиск..." v-model="searchQuery">-->
+<!--      </div>-->
+<!--      <div class="col-md-2">-->
+<!--        <select class="form-select" v-model="selectedStatus">-->
+<!--          <option value="">Все статусы</option>-->
+<!--          <option value="К">К</option>-->
+<!--          <option value="И">И</option>-->
+<!--          <option value="Ч">Ч</option>-->
+<!--          <option value="М">М</option>-->
+<!--          <option value="КП">КП</option>-->
+<!--        </select>-->
+<!--      </div>-->
+<!--      <div class="col-md-2">-->
+<!--        <input type="date" class="form-control" v-model="startDate">-->
+<!--      </div>-->
+<!--      <div class="col-md-2">-->
+<!--        <input type="date" class="form-control" v-model="endDate">-->
+<!--      </div>-->
+<!--      <div class="col-md-3">-->
+<!--        <button class="btn btn-primary" @click="applyFilters">Применить</button>-->
+<!--      </div>-->
+<!--    </div>-->
     <table id="ordersTable" class="table table-striped">
       <tbody>
       </tbody>
@@ -9,19 +33,24 @@
 
 <script>
 import DataTable from 'datatables.net-dt';
-import {onMounted, ref, watch} from 'vue';
-import {useHomeStore} from '../store/home.module';
-import {getOrders} from '../api/orders';
+import { onMounted, ref, watch } from 'vue';
+import { useHomeStore } from '../store/home.module';
+import { getOrders } from '../api/orders';
 
 // Импортируем стили DataTables и Bootstrap 5
 import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
 import 'datatables.net-bs5';
-import {LANGUAGE_CONFIG, ORDERS_TABLE_COLUMNS} from "../constants.js";
+import { LANGUAGE_CONFIG, ORDERS_TABLE_COLUMNS } from "../constants.js";
 
 export default {
   setup() {
     const homeStore = useHomeStore();
     const ordersTable = ref(null);
+
+    const searchQuery = ref(homeStore.searchQuery);
+    const selectedStatus = ref('');
+    const startDate = ref('');
+    const endDate = ref('');
 
     const initDataTable = () => {
       ordersTable.value = new DataTable('#ordersTable', {
@@ -29,7 +58,8 @@ export default {
         serverSide: true,
         ajax: (data, callback, settings) => {
           const page = Math.floor(data.start / data.length) + 1;
-          getOrders(page, data.length, homeStore.searchQuery)
+
+          getOrders(page, data.length, searchQuery.value, selectedStatus.value, startDate.value, endDate.value)
               .then(response => {
                 callback({
                   data: response.orders,
@@ -46,9 +76,13 @@ export default {
       });
     };
 
-    watch(() => homeStore.searchQuery, () => {
+    watch([searchQuery, selectedStatus, startDate, endDate], () => {
       ordersTable.value.ajax.reload();
     });
+
+    const applyFilters = () => {
+      ordersTable.value.ajax.reload();
+    };
 
     onMounted(() => {
       initDataTable();
@@ -56,6 +90,11 @@ export default {
 
     return {
       ordersTable,
+      searchQuery,
+      selectedStatus,
+      startDate,
+      endDate,
+      applyFilters
     };
   },
 };
