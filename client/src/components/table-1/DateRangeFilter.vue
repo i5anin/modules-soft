0acd4d2994f5709560a9aeb8d5d5c9f57ecfc495
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-09-24 14:42:22 +0300

    +

--- client/src/components/table-1/DateRangeFilter.vue
+++ client/src/components/table-1/DateRangeFilter.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="row mb-3">
     <div class="col-md-6">
-      <label for="dateRange" class="form-label">Выберите диапазон дат:</label>
-      <input type="text" class="form-control" id="dateRange" />
+      <label for="singleDate" class="form-label">Выберите дату:</label>
+      <Datepicker
+          id="singleDate"
+          v-model="selectedDate"
+          :enableTimePicker="false"
+          :format="dateFormat"
+          :locale="ruLocale"
+          :calendar-class="'custom-calendar'"
+          :clearable="true"
+      />
     </div>
   </div>
 </template>
@@ -13,14 +21,34 @@ import moment from 'moment';
 import 'bootstrap-daterangepicker/daterangepicker.css';
 import 'bootstrap-daterangepicker';
 import localeSettings from './daterangepicker-locale.js';
+import Datepicker from 'vue3-datepicker';
+import { ref } from 'vue';
+import { ru } from 'date-fns/locale';
 
 export default {
+  components: {
+    Datepicker,
+  },
+  setup() {
+    const selectedDate = ref(new Date());
+    const dateFormat = 'dd.MM.yyyy';
+    const ruLocale = ru;
+
+    return {
+      selectedDate,
+      dateFormat,
+      ruLocale,
+    };
+  },
   mounted() {
     this.initializeDateRangePicker();
   },
   methods: {
     initializeDateRangePicker() {
-      $('#dateRange').daterangepicker({
+      // Убедитесь, что moment импортирован и используется правильно
+      console.log(typeof moment); // Проверка, что moment доступен и является функцией
+
+      $('input[name="daterange"]').daterangepicker({
         showDropdowns: true,
         autoApply: true,
         locale: localeSettings,
@@ -34,3 +62,10 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+/* Добавьте стили, если необходимо */
+.custom-calendar {
+  /* Добавьте стили для календаря, если необходимо */
+}
+</style>
