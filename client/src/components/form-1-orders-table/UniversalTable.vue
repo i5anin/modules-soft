11814073e8ca2b76577d098b4cd9d455d80d2d2b
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-10-11 11:21:37 +0300

    Create UniversalTable.vue

---
+++ client/src/components/form-1-orders-table/UniversalTable.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <pre>{{ data }}</pre>
+    <pre>{{ console.log(data) }}</pre>
+    <table class="table table-striped table-bordered">
+      <thead>
+      <tr>
+        <th v-for="field in fields"
+            :key="field.name">
+          {{ field.title }}
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(item, index) in data" :key="index">
+        <td v-for="field in fields"
+            :key="field.name">
+          {{ item[field.name] }}
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UniversalTable',
+  props: {
+    fields: {
+      type: Array,
+      required: true
+    },
+    data: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Дополнительные стили, если необходимо */
+</style>
