9360ae77121a1accd77da3a5b31083286c359b49
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-10-14 13:21:57 +0300

    Update UniversalTable.vue

--- client/src/components/DataTable/UniversalTable.vue
+++ client/src/components/DataTable/UniversalTable.vue
@@ -16,6 +16,7 @@ import DataTable from 'datatables.net-vue3'
 import DataTablesCore from 'datatables.net'
 import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
 import 'datatables.net-bs5';
+import _ from 'lodash'
 
 DataTable.use(DataTablesCore)
 
@@ -33,8 +34,9 @@ const tableRef = ref(null)
 const dataTableOptions = ref({
   processing: true,
   serverSide: true,
-  ajax: function (data, callback) {
-    props.urlDate(1, 15, '', '', '', props.startDate, props.endDate).then(response => {
+  ajax: async function (data, callback) {
+    try {
+      const response = await props.urlDate(1, 15, '', '', '', props.startDate, props.endDate)
       const responseData = response.table
       if (responseData) {
         headers.value = responseData.fields.map(field => ({
@@ -52,32 +54,29 @@ const dataTableOptions = ref({
         });
         dataLoaded.value = true
       }
-    })
+    } catch (error) {
+      console.error('Ошибка при загрузке данных:', error);
+      dataLoaded.value = false;
+    }
   },
-  language: {url: 'Russian.json'},
+  language: { url: 'Russian.json' },
 })
 
-const loadData = async () => {
-  const response = await props.urlDate(1, 15, '', '', '', props.startDate, props.endDate)
-  const responseData = response.table
-
-  if (responseData) {
-    headers.value = responseData.fields.map(field => ({
-      name: field.name,
-      title: field.title || field.name
-    }));
-    formattedData.value = responseData.data.map(item => {
-      return headers.value.map(header => item[header.name]);
-    });
-
-    if (tableRef.value) {
-      tableRef.value.datatable.clear().rows.add(formattedData.value).draw();
-    }
-
-    dataLoaded.value = true
+const debouncedReloadTable = _.debounce(() => {
+  if (tableRef.value) {
+    tableRef.value.datatable.ajax.reload(null, false);
   }
-}
+}, 500); // 500 мс задержка для уменьшения частоты запросов
 
-watch([() => props.startDate, () => props.endDate], loadData)
-onMounted(loadData)
+watch([() => props.startDate, () => props.endDate], () => {
+  if (dataLoaded.value) {
+    debouncedReloadTable();
+  }
+});
+
+onMounted(() => {
+  if (tableRef.value) {
+    tableRef.value.datatable.ajax.reload();
+  }
+});
 </script>
