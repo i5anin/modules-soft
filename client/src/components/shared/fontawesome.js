0222b8ee7d3b56de5ec7f06c10a8ebc6bca5ab08
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-10-15 15:52:00 +0300

    Revert "-"
    This reverts commit e408635db9cb44d2b9dd3f26d9f94bac2606c118.

--- client/src/components/shared/fontawesome.js
+++ client/src/components/shared/fontawesome.js
@@ -1,10 +1,8 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { faXmark, faCalendarDays, faSquareMinus, faSquarePlus } from '@fortawesome/free-solid-svg-icons';
+import { faCalendar } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import { faCalendarDays, faXmark } from '@fortawesome/free-solid-svg-icons'; // "solid"
-import { faCirclePlus, faCircleMinus, faCalendar } from '@fortawesome/free-regular-svg-icons'; // "regular"
-
-
-library.add(faXmark, faCalendarDays, faSquareMinus, faSquarePlus, faCalendar); // Добавляем обе версии иконок
+library.add(faXmark, faCalendarDays, faSquareMinus, faSquarePlus, faCalendar);
 
 export { FontAwesomeIcon };
