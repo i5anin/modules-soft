df8a068d137e9728eef9cb6b8cd16df046248ce7
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-10-15 16:43:42 +0300

    Update fontawesome.js

--- client/src/components/shared/fontawesome.js
+++ client/src/components/shared/fontawesome.js
@@ -1,10 +1,10 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import { faCalendarDays, faXmark, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'; // Используем solid версию иконок
+import { faCalendarDays, faXmark, faPlus, faMinus, faCubes, faCube } from '@fortawesome/free-solid-svg-icons'; // Добавляем нужные иконки
 import { faCalendar } from '@fortawesome/free-regular-svg-icons';
 
 // Добавляем необходимые иконки в библиотеку
-library.add(faXmark, faCalendarDays, faPlus, faMinus, faCalendar);
+library.add(faXmark, faCalendarDays, faPlus, faMinus, faCubes, faCube, faCalendar);
 
 export { FontAwesomeIcon };
