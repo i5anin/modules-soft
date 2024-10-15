fff61eb4f61c93b720370bf3b3a0804f21054962
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-10-15 10:36:53 +0300

    стили модалки

--- client/src/components/modal/Modal.vue
+++ client/src/components/modal/Modal.vue
@@ -4,7 +4,7 @@
     <div class="modal-dialog modal-xl"> <!-- Увеличиваем размер модального окна -->
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="orderModalLabel">Детали заказа</h5>
+          <h5 class="modal-title" id="orderModalLabel">Детали заказа </h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
                   @click="closeModal"></button>
         </div>
@@ -15,12 +15,12 @@
                 <div class="card-body">
                   <div class="row">
                     <div class="col-6">
-                      <div v-for="(field, index) in leftColumnFields" :key="index" class="mb-3">
+                      <div v-for="(field, index) in leftColumnFields" :key="index" class="mb-1">
                         <strong>{{ field.title }}:</strong> {{ selectedOrder.header.data[0][field.name] }}
                       </div>
                     </div>
                     <div class="col-6">
-                      <div v-for="(field, index) in rightColumnFields" :key="index" class="mb-3">
+                      <div v-for="(field, index) in rightColumnFields" :key="index" class="mb-1">
                         <strong>{{ field.title }}:</strong> {{ selectedOrder.header.data[0][field.name] }}
                       </div>
                     </div>
