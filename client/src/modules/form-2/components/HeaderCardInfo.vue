a4e67d2c224b6eb5eb2b2b7699e54dd45fd471ca
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-10-25 16:45:11 +0300

    Update HeaderCardInfo.vue

--- client/src/modules/form-2/components/HeaderCardInfo.vue
+++ client/src/modules/form-2/components/HeaderCardInfo.vue
@@ -1,43 +1,84 @@
 <template>
   <div>
-    <div v-if="header" class="card mb-3">
-      <div class="card-header">
-        <!-- Иконка mdi-format-list-bulleted-type через <span> -->
+    <div v-if="header" class="card mb-2">
+      <div class="card-header p-2">
         <span class="mdi mdi-format-list-bulleted-type"></span>
-
-        <!-- Иконка через компонент SvgIcon -->
         <svg-icon type="mdi" :path="mdiFormatListBulletedType" color="red" />
-
         Информация о заказе
       </div>
-      <div class="card-body">
+      <div class="card-body p-2">
         <div class="row">
+          <!-- Левая колонка -->
           <div class="col-md-6">
-            <p v-for="field in leftFields" :key="field.name" class="card-text">
-              <strong>{{ field.title }}:</strong>
-              {{ formatValue(header.data[field.name], field.name) }}
-            </p>
+            <table class="table table-sm">
+              <tbody>
+                <tr v-for="field in leftFields" :key="field.name">
+                  <td class="p-1">
+                    <strong>{{ field.title }}</strong>
+                  </td>
+                  <td class="p-1">
+                    <input
+                      type="text"
+                      v-model="fieldValues[field.name]"
+                      :placeholder="field.title"
+                      class="form-control form-control-sm"
+                      disabled
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
+
+          <!-- Правая колонка -->
           <div class="col-md-6">
-            <p v-for="field in rightFields" :key="field.name" class="card-text">
-              <strong>{{ field.title }}:</strong>
-              {{ formatValue(header.data[field.name], field.name) }}
-            </p>
+            <table class="table table-sm">
+              <tbody>
+                <tr v-for="field in rightFields" :key="field.name">
+                  <td class="p-1">
+                    <strong>{{ field.title }}</strong>
+                  </td>
+                  <td class="p-1">
+                    <input
+                      aria-label="Small"
+                      type="text"
+                      v-model="fieldValues[field.name]"
+                      :placeholder="field.title"
+                      class="form-control form-control-sm"
+                      disabled
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
-        <div v-if="hasNonEmptyComments" class="card mt-3">
-          <div class="card-body">
-            <template
-              v-for="commentField in commentFields"
-              :key="commentField.name"
-            >
-              <p v-if="header.data[commentField.name]" class="card-text">
-                <span>
-                  <strong>{{ commentField.title }}:</strong>
-                  {{ header.data[commentField.name] }}
-                </span>
-              </p>
-            </template>
+
+        <!-- Комментарии, если они есть -->
+        <div v-if="hasNonEmptyComments" class="card mt-2">
+          <div class="card-body p-2">
+            <table class="table table-sm">
+              <tbody>
+                <tr
+                  v-for="commentField in commentFields"
+                  :key="commentField.name"
+                >
+                  <td>
+                    <strong>{{ commentField.title }}</strong>
+                  </td>
+                  <td>
+                    <textarea
+                      class="form-control"
+                      aria-label="textarea"
+                      type="text"
+                      v-model="fieldValues[commentField.name]"
+                      :placeholder="commentField.title"
+                      disabled
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
@@ -46,7 +87,7 @@
 </template>
 
 <script>
-import { computed, defineComponent, ref } from 'vue'
+import { computed, defineComponent, ref, reactive } from 'vue'
 import SvgIcon from '@jamescoyle/vue-icon'
 import { mdiFormatListBulletedType } from '@mdi/js'
 import { formatValue as formatterFormatValue } from '@/utils/formatters.js'
@@ -61,14 +102,6 @@ export default defineComponent({
   },
   setup(props) {
     const mdiFormatListBulletedTypeRef = ref(mdiFormatListBulletedType)
-
-    const hasNonEmptyComments = computed(() =>
-      commentFields.value.some(
-        (field) =>
-          props.header.data[field.name] &&
-          props.header.data[field.name].trim() !== ''
-      )
-    )
 
     const commentFields = computed(() =>
       props.header.fields
@@ -95,20 +128,36 @@ export default defineComponent({
       nonCommentFields.value.slice(Math.ceil(nonCommentFields.value.length / 2))
     )
 
-    // Определение функции formatValue в setup
-    const formatValue = (value, fieldName) => {
-      return formatterFormatValue(value, fieldName)
-    }
+    const hasNonEmptyComments = computed(() =>
+      commentFields.value.some(
+        (field) =>
+          props.header.data[field.name] &&
+          props.header.data[field.name].trim() !== ''
+      )
+    )
+
+    // Состояние для значений полей
+    const fieldValues = reactive(
+      Object.fromEntries(
+        [...nonCommentFields.value, ...commentFields.value].map((field) => [
+          field.name,
+          formatterFormatValue(props.header.data[field.name], field.name),
+        ])
+      )
+    )
 
     return {
       mdiFormatListBulletedType: mdiFormatListBulletedTypeRef,
-      hasNonEmptyComments,
-      commentFields,
-      nonCommentFields,
       leftFields,
       rightFields,
-      formatValue,
+      hasNonEmptyComments,
+      commentFields,
+      fieldValues,
     }
   },
 })
 </script>
+
+<style scoped>
+/* Ваши стили */
+</style>
