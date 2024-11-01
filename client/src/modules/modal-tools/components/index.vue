cc79d62bc6c9b4511c64088c2c38bad065e5f29d
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-11-01 16:58:20 +0300

    feat(modal-tools): изменено расположение таблиц на боковое и увеличен размер модального окна
    - Таблицы "Добавленные инструменты" и "Предложенный инструмент" теперь отображаются рядом друг с другом (слева и справа).
    - Модальное окно расширено до полноэкранного режима для лучшего отображения информации.
    - Добавлена функция генерации уникального идентификатора для инструментов.
    - Исправлено исчезновение всех позиций при добавлении инструмента.

--- client/src/modules/modal-tools/components/index.vue
+++ client/src/modules/modal-tools/components/index.vue
@@ -7,7 +7,7 @@
     aria-hidden="true"
     ref="modalContainer"
   >
-    <div class="modal-dialog modal-xl">
+    <div class="modal-dialog modal-fullscreen">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="orderModalLabel">
@@ -23,185 +23,203 @@
         </div>
         <div class="modal-body">
           <div v-if="instrumentData">
-            <!-- Временная таблица добавленного инструмента -->
-            <div v-if="addedInstruments.length > 0">
-              <h5>Добавленные инструменты:</h5>
-              <table
-                class="table table-sm table-bordered table-hover table-striped mt-3"
-              >
-                <thead>
-                  <tr class="text-center">
-                    <th style="width: 1%">#</th>
-                    <th style="width: 1%">Т шт.</th>
-                    <th style="width: 20%">
-                      Расположение в дереве инструмента
-                    </th>
-                    <th style="width: 20%">Характеристики</th>
-                    <th style="width: 1%">Удалить</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr
-                    v-for="(item, index) in addedInstruments"
-                    :key="'added-' + index"
+            <div class="row">
+              <div class="col-md-6">
+                <!-- Временная таблица добавленного инструмента -->
+                <div v-if="addedInstruments.length > 0">
+                  <h5>Добавленные инструменты:</h5>
+                  <table
+                    class="table table-sm table-bordered table-hover table-striped mt-3"
                   >
-                    <td class="text-center align-middle">{{ index + 1 }}</td>
-                    <td class="text-center align-middle">{{ item.t_op }}</td>
-                    <td>
-                      <ul class="list-group list-group-flush">
-                        <li
-                          class="list-group-item"
-                          v-for="(segment, segmentIndex) in item.path.split(
-                            ' > '
-                          )"
-                          :key="segmentIndex"
-                        >
-                          {{ segment }}
-                        </li>
-                      </ul>
-                    </td>
-                    <td>
-                      <ul
-                        class="list-group list-group-flush list-group-item-success"
+                    <thead>
+                      <tr class="text-center">
+                        <th style="width: 1%">#</th>
+                        <th style="width: 1%">Т шт.</th>
+                        <th style="width: 20%">
+                          Расположение в дереве инструмента
+                        </th>
+                        <th style="width: 20%">Характеристики</th>
+                        <th style="width: 1%">Удалить</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr
+                        v-for="(item, index) in addedInstruments"
+                        :key="generateUniqueId(item)"
                       >
-                        <li
-                          class="list-group-item"
-                          v-for="(value, key) in item.property_description"
-                          :key="key"
-                        >
-                          {{ key }}: <b>{{ value }}</b>
-                        </li>
-                      </ul>
-                    </td>
-                    <td class="text-center align-middle">
-                      <button
-                        type="button"
-                        class="btn btn-circle"
-                        @click="removeInstrument(index)"
-                      >
-                        <svg-icon type="mdi" :path="mdiDelete" color="red" />
-                      </button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-
-            <!-- Исходная таблица инструментов -->
-            <h5>Предложенный инструмент:</h5>
-            <table
-              class="table table-sm table-bordered table-hover table-striped mt-4"
-            >
-              <thead>
-                <tr class="text-center">
-                  <th style="width: 1%">#</th>
-                  <th style="width: 1%">Т шт.</th>
-                  <th style="width: 30%">Комментарии оператора</th>
-                  <th style="width: 20%">Расположение в дереве инструмента</th>
-                  <th style="width: 20%">Характеристики</th>
-                  <th style="width: 1%">Добавить</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr
-                  v-for="(item, index) in instrumentData.data.slice(0, 5)"
-                  :key="index"
+                        <td class="text-center align-middle">
+                          {{ index + 1 }}
+                        </td>
+                        <td class="text-center align-middle">
+                          {{ item.t_op }}
+                        </td>
+                        <td>
+                          <ul class="list-group list-group-flush">
+                            <li
+                              class="list-group-item"
+                              v-for="(segment, segmentIndex) in item.path.split(
+                                ' > '
+                              )"
+                              :key="segmentIndex"
+                            >
+                              {{ segment }}
+                            </li>
+                          </ul>
+                        </td>
+                        <td>
+                          <ul
+                            class="list-group list-group-flush list-group-item-success"
+                          >
+                            <li
+                              class="list-group-item"
+                              v-for="(value, key) in item.property_description"
+                              :key="key"
+                            >
+                              {{ key }}: <b>{{ value }}</b>
+                            </li>
+                          </ul>
+                        </td>
+                        <td class="text-center align-middle">
+                          <button
+                            type="button"
+                            class="btn btn-circle"
+                            @click="removeInstrument(item)"
+                          >
+                            <svg-icon
+                              type="mdi"
+                              :path="mdiDelete"
+                              color="red"
+                            />
+                          </button>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <!-- Исходная таблица инструментов -->
+                <h5>Предложенный инструмент:</h5>
+                <table
+                  class="table table-sm table-bordered table-hover table-striped mt-4"
                 >
-                  <td class="text-center align-middle">{{ index + 1 }}</td>
-                  <td class="text-center align-middle">{{ item.t_op }}</td>
-                  <td>
-                    <ul class="list-group list-group-flush mt-2">
-                      <!-- Показываем первые три комментария по умолчанию -->
-                      <li
-                        class="list-group-item"
-                        v-for="(
-                          comment, cIndex
-                        ) in item.comments_operators.slice(0, 3)"
-                        :key="cIndex"
-                        style="font-size: 13px"
-                      >
-                        {{ comment }}
-                      </li>
-                    </ul>
-
-                    <!-- Кнопка для раскрытия оставшихся комментариев, отображаемая под списком -->
-                    <div
-                      v-if="item.comments_operators.length > 3"
-                      class="text-center mt-2"
+                  <thead>
+                    <tr class="text-center">
+                      <th style="width: 1%">#</th>
+                      <th style="width: 1%">Т шт.</th>
+                      <th style="width: 30%">Комментарии оператора</th>
+                      <th style="width: 20%">
+                        Расположение в дереве инструмента
+                      </th>
+                      <th style="width: 20%">Характеристики</th>
+                      <th style="width: 1%">Добавить</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr
+                      v-for="(item, index) in filteredInstruments"
+                      :key="generateUniqueId(item)"
                     >
-                      <button
-                        class="btn btn-sm btn-outline-secondary"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        :data-bs-target="'#collapseComments' + index"
-                        aria-expanded="false"
-                        :aria-controls="'collapseComments' + index"
-                        style="font-size: 12px"
-                      >
-                        Показать остальные
-                      </button>
-                      <div
-                        :id="'collapseComments' + index"
-                        class="collapse mt-2"
-                      >
-                        <ul class="list-group list-group-flush">
+                      <td class="text-center align-middle">{{ index + 1 }}</td>
+                      <td class="text-center align-middle">{{ item.t_op }}</td>
+                      <td>
+                        <ul class="list-group list-group-flush mt-2">
+                          <!-- Показываем первые три комментария по умолчанию -->
                           <li
                             class="list-group-item"
                             v-for="(
-                              comment, subIndex
-                            ) in item.comments_operators.slice(3)"
-                            :key="subIndex"
-                            style="font-size: 13px"
+                              comment, cIndex
+                            ) in item.comments_operators.slice(0, 3)"
+                            :key="cIndex"
+                            style="font-size: 12px; padding: 2px"
                           >
                             {{ comment }}
                           </li>
                         </ul>
-                      </div>
-                    </div>
-                  </td>
 
-                  <td>
-                    <ul class="list-group list-group-flush">
-                      <li
-                        class="list-group-item"
-                        v-for="(segment, segmentIndex) in item.path.split(
-                          ' > '
-                        )"
-                        :key="segmentIndex"
-                      >
-                        {{ segment }}
-                      </li>
-                    </ul>
-                  </td>
-                  <td>
-                    <ul
-                      class="list-group list-group-flush list-group-item-warning"
-                    >
-                      <li
-                        class="list-group-item"
-                        v-for="(value, key) in item.property_description"
-                        :key="key"
-                      >
-                        {{ key }}: <b>{{ value }}</b>
-                      </li>
-                    </ul>
-                  </td>
-                  <td class="text-center align-middle">
-                    <button
-                      type="button"
-                      class="btn btn-circle"
-                      @click="addInstrument(item)"
-                    >
-                      <svg-icon
-                        type="mdi"
-                        :path="mdiPlusCircle"
-                        color="green"
-                      />
-                    </button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+                        <!-- Кнопка для раскрытия оставшихся комментариев -->
+                        <div
+                          v-if="item.comments_operators.length > 3"
+                          class="text-center mt-2"
+                        >
+                          <button
+                            class="btn btn-sm btn-outline-secondary"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            :data-bs-target="
+                              '#collapseComments' + generateUniqueId(item)
+                            "
+                            aria-expanded="false"
+                            :aria-controls="
+                              'collapseComments' + generateUniqueId(item)
+                            "
+                          >
+                            Показать остальные
+                          </button>
+                          <div
+                            :id="'collapseComments' + generateUniqueId(item)"
+                            class="collapse mt-2"
+                          >
+                            <ul class="list-group list-group-flush">
+                              <li
+                                class="list-group-item"
+                                v-for="(
+                                  comment, subIndex
+                                ) in item.comments_operators.slice(3)"
+                                :key="subIndex"
+                                style="font-size: 13px"
+                              >
+                                {{ comment }}
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </td>
+
+                      <td>
+                        <ul class="list-group list-group-flush">
+                          <li
+                            class="list-group-item"
+                            v-for="(segment, segmentIndex) in item.path.split(
+                              ' > '
+                            )"
+                            :key="segmentIndex"
+                          >
+                            {{ segment }}
+                          </li>
+                        </ul>
+                      </td>
+                      <td>
+                        <ul
+                          class="list-group list-group-flush list-group-item-warning"
+                        >
+                          <li
+                            class="list-group-item"
+                            v-for="(value, key) in item.property_description"
+                            :key="key"
+                          >
+                            {{ key }}: <b>{{ value }}</b>
+                          </li>
+                        </ul>
+                      </td>
+                      <td class="text-center align-middle">
+                        <button
+                          type="button"
+                          class="btn btn-circle"
+                          @click="addInstrument(item)"
+                        >
+                          <svg-icon
+                            type="mdi"
+                            :path="mdiPlusCircle"
+                            color="green"
+                          />
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
           <div v-else>
             <p>Загрузка данных...</p>
@@ -223,7 +241,7 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import { Modal } from 'bootstrap'
 import { fetchInstrumentData } from '../api/tools.js'
 import { useRoute } from 'vue-router'
@@ -258,14 +276,32 @@ const fetchData = async () => {
   }
 }
 
-// Функция для добавления инструмента в временную таблицу
+// Функция для генерации уникального идентификатора
+const generateUniqueId = (item) => {
+  return `${item.tool_group_id}-${JSON.stringify(item.property_description)}`
+}
+
+// Вычисляемое свойство для фильтрации инструментов
+const filteredInstruments = computed(() => {
+  if (!instrumentData.value) return []
+  return instrumentData.value.data.filter((item) => {
+    const itemId = generateUniqueId(item)
+    return !addedInstruments.value.some(
+      (addedItem) => generateUniqueId(addedItem) === itemId
+    )
+  })
+})
+
+// Функция для добавления инструмента
 const addInstrument = (item) => {
   addedInstruments.value.push(item)
 }
 
-// Функция для удаления инструмента из временной таблицы
-const removeInstrument = (index) => {
-  addedInstruments.value.splice(index, 1)
+// Функция для удаления инструмента
+const removeInstrument = (item) => {
+  addedInstruments.value = addedInstruments.value.filter(
+    (addedItem) => generateUniqueId(addedItem) !== generateUniqueId(item)
+  )
 }
 
 // Функция для закрытия модального окна
@@ -292,5 +328,21 @@ const closeModal = () => {
 
 .btn-circle:hover {
   background-color: transparent;
+}
+
+@media (max-width: 767px) {
+  .col-md-6 {
+    flex: 0 0 100%;
+    max-width: 100%;
+  }
+}
+
+.modal-fullscreen {
+  max-width: 100%;
+  margin: 0;
+}
+
+.modal-content {
+  height: 100vh;
 }
 </style>
