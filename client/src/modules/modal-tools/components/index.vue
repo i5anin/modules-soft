e79fd735af14c308f0e1e7e86322bf88ec00b926
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-11-01 16:04:58 +0300

    feat(modal-tools): улучшено отображение комментариев и добавлена кнопка "Добавить"
    - Комментарии оператора теперь отображаются в виде списка с показом первых трёх и возможностью раскрыть остальные.
    - Добавлена новая колонка "Добавить" с кнопкой "+" для добавления инструмента в временную таблицу.
    - Обновлено заголовок модального окна для более информативного отображения операции и её уникального ID.

--- client/src/modules/modal-tools/components/index.vue
+++ client/src/modules/modal-tools/components/index.vue
@@ -11,7 +11,7 @@
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="orderModalLabel">
-            Модальное окно инструмента {{ no }} {{ nomId }}
+            Инструмент (операция {{ no }}, уникальный id операции {{ nomId }})
           </h5>
           <button
             type="button"
@@ -23,15 +23,17 @@
         </div>
         <div class="modal-body">
           <div v-if="instrumentData">
-            <table class="table table-sm table-bordered table-hover">
+            <table
+              class="table table-sm table-bordered table-hover table-striped"
+            >
               <thead>
                 <tr class="text-center">
-                  <!-- Центрируем заголовки -->
                   <th style="width: 1%">#</th>
-                  <th style="width: 10%">Т шт. операции</th>
-                  <th style="width: 15%">Комментарии оператора</th>
-                  <th style="width: 20%">Расположение в дереве инсрумента</th>
-                  <th style="width: 25%">Характеристики</th>
+                  <th style="width: 1%">Т шт.</th>
+                  <th style="width: 30%">Комментарии оператора</th>
+                  <th style="width: 20%">Расположение в дереве инструмента</th>
+                  <th style="width: 20%">Характеристики</th>
+                  <th style="width: 1%">Добавить</th>
                 </tr>
               </thead>
               <tbody>
@@ -39,9 +41,59 @@
                   v-for="(item, index) in instrumentData.data.slice(0, 5)"
                   :key="index"
                 >
-                  <td>{{ index + 1 }}</td>
-                  <td>{{ item.t_op }}</td>
-                  <td>{{ item.comments_operators }}</td>
+                  <td class="text-center align-middle">{{ index + 1 }}</td>
+                  <td class="text-center align-middle">{{ item.t_op }}</td>
+                  <td>
+                    <ul class="list-group list-group-flush mt-2">
+                      <!-- Показываем первые три комментария по умолчанию -->
+                      <li
+                        class="list-group-item"
+                        v-for="(
+                          comment, index
+                        ) in item.comments_operators.slice(0, 3)"
+                        :key="index"
+                        style="font-size: 13px"
+                      >
+                        {{ comment }}
+                      </li>
+                    </ul>
+
+                    <!-- Кнопка для раскрытия оставшихся комментариев, отображаемая под списком -->
+                    <div
+                      v-if="item.comments_operators.length > 3"
+                      class="text-center mt-2"
+                    >
+                      <button
+                        class="btn btn-sm btn-outline-secondary"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        :data-bs-target="'#collapseComments' + index"
+                        aria-expanded="false"
+                        :aria-controls="'collapseComments' + index"
+                        style="font-size: 12px"
+                      >
+                        Показать остальные
+                      </button>
+                      <div
+                        :id="'collapseComments' + index"
+                        class="collapse mt-2"
+                      >
+                        <ul class="list-group list-group-flush">
+                          <li
+                            class="list-group-item"
+                            v-for="(
+                              comment, subIndex
+                            ) in item.comments_operators.slice(3)"
+                            :key="subIndex"
+                            style="font-size: 13px"
+                          >
+                            {{ comment }}
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </td>
+
                   <td>
                     <ul class="list-group list-group-flush">
                       <li
@@ -67,6 +119,11 @@
                         {{ key }}: <b>{{ value }}</b>
                       </li>
                     </ul>
+                  </td>
+                  <td class="text-center align-middle">
+                    <button type="button" class="btn btn-circle">
+                      <svg-icon type="mdi" :path="path" color="green" />
+                    </button>
                   </td>
                 </tr>
               </tbody>
@@ -96,17 +153,19 @@ import { onMounted, ref } from 'vue'
 import { Modal } from 'bootstrap'
 import { fetchInstrumentData } from '../api/tools.js'
 import { useRoute } from 'vue-router'
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiPlusCircle } from '@mdi/js'
 
 const route = useRoute()
 
 const no = ref(route.params.no)
 const nomId = ref(route.params.nomId)
-
 const emit = defineEmits(['close'])
 
 const modalContainer = ref(null)
 let modalInstance = null
 const instrumentData = ref(null)
+const path = mdiPlusCircle // Иконка
 
 onMounted(() => {
   modalInstance = new Modal(modalContainer.value)
@@ -123,11 +182,6 @@ const fetchData = async () => {
       instrumentData.value = null
     }
   }
-}
-
-const closeModal = () => {
-  modalInstance.hide()
-  emit('close')
 }
 </script>
 
