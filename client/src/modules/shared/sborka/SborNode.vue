e4e4c40b5c30cdd99a2daf23d1b2b08786bbb565
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-11-13 16:55:33 +0300

    feat(sborka): добавлено отображение веток связей в SborNode.vue
    - Реализовано визуальное отображение веток между узлами для представления иерархической структуры.
    - Сохранена функциональность разворачивания/сворачивания узлов с помощью иконок плюса и минуса.
    - Добавлены стили CSS для отображения вертикальных и горизонтальных линий, соединяющих узлы.
    - Обновлен `SborNode.vue` с учетом этих изменений.

--- client/src/modules/shared/sborka/SborNode.vue
+++ client/src/modules/shared/sborka/SborNode.vue
@@ -4,55 +4,67 @@
     :class="{ 'table-info': isExpanded, 'fw-bold': isExpanded }"
     style="table-layout: fixed"
   >
+    <!-- Иконка разворачивания/сворачивания -->
     <td :style="{ width: '40px', textAlign: 'center' }">
-      <span v-if="hasChildren" title="развернуть/свернуть">
+      <span v-if="hasChildren" title="Развернуть/Свернуть">
         <font-awesome-icon
           :icon="isExpanded ? ['fas', 'minus'] : ['fas', 'plus']"
           class="icon-sm"
         />
       </span>
     </td>
+
+    <!-- Статусы -->
     <td :style="{ width: '40px', textAlign: 'center' }">
       <span v-html="combinedStatuses" style="display: inline-flex"></span>
     </td>
-    <td
-      v-for="field in fieldsArray"
-      :key="field.name"
-      style="cursor: pointer; font-size: 12px"
-    >
-      <span
+
+    <!-- Поля -->
+    <td v-for="field in fieldsArray" :key="field.name" style="font-size: 12px">
+      <div
         v-if="field === firstField"
-        :style="{
-          paddingLeft: `${depth * 35}px`,
-          display: 'flex',
-          alignItems: 'center',
-        }"
+        class="tree-node"
+        :style="{ paddingLeft: `${depth * 20}px`, position: 'relative' }"
       >
-        <font-awesome-icon
-          :icon="sbor.is_sbor ? ['fas', 'cubes'] : ['fas', 'cube']"
-          :style="{ color: sbor.is_sbor ? '#dc6611' : '#cfa614' }"
-          class="icon-sm ms-2 me-2"
-        />
-        <span>{{ formatValue(sbor[field.name], field.type) }}</span>
-        <font-awesome-icon
-          :icon="['fas', 'circle-info']"
-          :style="{ color: 'green' }"
-          class="icon-sm ms-2 me-2"
-          @click="handleRowClick"
-        />
-      </span>
+        <!-- Ветвь связей -->
+        <div
+          v-if="depth > 0"
+          class="branch-line"
+          :class="{ 'last-child': isLastChild }"
+          :style="{ left: `${(depth - 1) * 20}px` }"
+        ></div>
+
+        <!-- Содержимое узла -->
+        <div class="node-content">
+          <font-awesome-icon
+            :icon="sbor.is_sbor ? ['fas', 'cubes'] : ['fas', 'cube']"
+            :style="{ color: sbor.is_sbor ? '#dc6611' : '#cfa614' }"
+            class="icon-sm me-2"
+          />
+          <span>{{ formatValue(sbor[field.name], field.type) }}</span>
+          <font-awesome-icon
+            :icon="['fas', 'circle-info']"
+            :style="{ color: 'green' }"
+            class="icon-sm ms-2 me-2"
+            @click.stop="handleRowClick"
+          />
+        </div>
+      </div>
       <span v-else :title="generateTitle(field)" style="font-size: 13px">
         {{ formatValue(sbor[field.name], field.type) }}
       </span>
     </td>
   </tr>
+
+  <!-- Рекурсивный вызов для дочерних узлов -->
   <template v-if="isExpanded && hasChildren">
     <SborNode
-      v-for="child in sbor.sbor_tree"
+      v-for="(child, index) in sbor.sbor_tree"
       :key="child.ordersnom_id"
       :sbor="child"
       :fields="fields"
       :depth="depth + 1"
+      :isLastChild="index === sbor.sbor_tree.length - 1"
     />
   </template>
 </template>
@@ -72,6 +84,7 @@ export default {
     sbor: { type: Object, required: true },
     depth: { type: Number, default: 0 },
     fields: { type: Array, default: () => [] },
+    isLastChild: { type: Boolean, default: false },
   },
   setup(props) {
     const sborStore = store()
@@ -115,7 +128,6 @@ export default {
     const generateTitle = (field) => `Поле: ${field.title || 'Нет данных'}`
 
     return {
-      formatValue,
       isExpanded,
       toggle,
       handleRowClick,
@@ -123,8 +135,53 @@ export default {
       firstField,
       combinedStatuses,
       hasChildren,
+      formatValue,
       generateTitle,
     }
   },
 }
 </script>
+
+<style scoped>
+.tree-node {
+  display: flex;
+  align-items: center;
+  position: relative;
+}
+
+.branch-line {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 20px;
+}
+
+.branch-line::before {
+  content: '';
+  position: absolute;
+  left: 10px;
+  top: 0;
+  bottom: 0;
+  width: 1px;
+  background-color: #989898;
+}
+
+.branch-line::after {
+  content: '';
+  position: absolute;
+  left: 10px;
+  top: 50%;
+  width: 10px;
+  height: 1px;
+  background-color: #989898;
+}
+
+.last-child .branch-line::before {
+  height: 50%;
+}
+
+.node-content {
+  display: flex;
+  align-items: center;
+}
+</style>
