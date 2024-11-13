e188f9213067a4c8d2d851f4c4b1d35347e05ab2
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-11-13 10:47:01 +0300

    fix(lint): попытка исправления правил eslint для props и других переменных
    - обновлён шаблон в id-match для добавления props, totalPg, и totalCnt;
    - props всё ещё вызывает ошибку id-match, несмотря на изменение регулярного выражения.

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -6,7 +6,15 @@ import pluginUnusedImports from 'eslint-plugin-unused-imports'
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs['flat/essential'],
@@ -19,6 +27,56 @@ export default [
       'unused-imports': pluginUnusedImports, // Указываем плагин в формате объекта
     },
     rules: {
+      'id-match': [
+        'warn',
+        '^(table(Data|Fields|Headers|Rows|Columns|Records|Status|Config|Info|Details|props))$',
+        {
+          properties: true,
+          ignoreDestructuring: true, // Игнорирует деструктурированные параметры, такие как 'props'
+          onlyDeclarations: true, // Проверяет только объявления переменных, игнорируя ключевые слова
+        },
+      ],
+      'no-restricted-imports': [
+        'warn',
+        {
+          paths: [
+            {
+              name: 'vue',
+              importNames: [
+                'props',
+                'ref',
+                'reactive',
+                'computed',
+                'watch',
+                'watchEffect',
+                'onMounted',
+                'onUpdated',
+                'onUnmounted',
+                'onBeforeMount',
+                'onBeforeUpdate',
+                'onBeforeUnmount',
+                'provide',
+                'inject',
+                'nextTick',
+                'defineComponent',
+                'toRef',
+                'toRefs',
+                'isRef',
+                'isReactive',
+                'isReadonly',
+                'readonly',
+                'shallowRef',
+                'shallowReactive',
+                'markRaw',
+                'customRef',
+                'unref',
+              ],
+              message:
+                'Импортируйте стандартные функции Vue 3 для использования в Composition API',
+            },
+          ],
+        },
+      ],
       'vue/multi-word-component-names': 'off',
       'max-lines': ['warn', { max: 350 }],
       complexity: ['warn', { max: 10 }],
