61f7c4d45d2970f02204185423fe8e91ec11d5e6
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-11-19 16:37:56 +0300

    refactor(ui): обновление компонентов для фильтрации по диапазону дат
    - Удалён компонент `DateRangeFilterComponent.vue` за ненадобностью.
    - Переименован компонент `DateInput.vue` для унификации именования.
    - Обновлена структура и подключение компонентов в `DateRangeFilters.vue`.

--- client/src/modules/shared/server-side/DateRangeFilters.vue
+++ client/src/modules/shared/server-side/DateRangeFilters.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="d-flex me-3">
     <div class="d-flex">
-      <label for="start-date" class="form-label fw-bold me-2 mb-0"
-        >Диапазон:</label
-      >
+      <label for="start-date" class="form-label fw-bold me-2 mb-0">
+        Диапазон:
+      </label>
       <DateRangeFilter id="start-date" :value="start" @input="updateStart" />
     </div>
     <div class="d-flex align-items-center ms-3">
-      <label for="end-date" class="form-label fw-bold me-2 mb-0">-</label>
+      <label for="end-date" class="form-label fw-bold me-2 mb-0"></label>
       <DateRangeFilter id="end-date" :value="end" @input="updateEnd" />
     </div>
   </div>
 </template>
 
 <script>
-import DateRangeFilter from '@/modules/shared/modules-server-side/DateRangeFilter.vue'
+import DateRangeFilter from '@/modules/shared/modules-server-side/DateInput.vue'
 
 export default {
   components: { DateRangeFilter },
