f00b16e6deae69be8ebccce54c4432f1f63ba0ee
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-11-25 10:16:20 +0300

    enh(navbar): добавлен компонент BackButton и селектор типа
    - Заменен заголовок модуля на компонент BackButton с отключаемым текстом.
    - Добавлен селектор типа с условным отображением при наличии данных.
    - Логика инициализации данных типа и роли синхронизирована с localStorage.
    - Обновлены обработчики изменения роли и типа для сохранения данных в localStorage.

--- client/src/modules/_main/components/Navbar.vue
+++ client/src/modules/_main/components/Navbar.vue
@@ -4,7 +4,7 @@
       class="container-fluid d-flex justify-content-between align-items-center"
     >
       <router-link class="navbar-brand mb-0 h1" to="/">
-        Модуль «Расчет Заказа {{ roleStore.roleDisplayName }}»
+        <BackButton :showText="false" />
       </router-link>
       <!-- Селектор роли -->
       <div class="d-flex align-items-center">
@@ -26,34 +26,37 @@
       </div>
 
       <!-- Селектор типа -->
-      <!--      <div class="d-flex align-items-center">-->
-      <!--        <label for="type-selector" class="me-2 mb-0">Выбор типа:</label>-->
-      <!--        <select-->
-      <!--          class="form-select form-select-sm w-auto"-->
-      <!--          id="type-selector"-->
-      <!--          v-model="roleStore.selectedTypes"-->
-      <!--          @change="onTypeChange"-->
-      <!--        >-->
-      <!--          <option-->
-      <!--            v-for="(type, key) in roleStore.types"-->
-      <!--            :key="key"-->
-      <!--            :value="type"-->
-      <!--          >-->
-      <!--            {{ type }}-->
-      <!--          </option>-->
-      <!--        </select>-->
-      <!--      </div>-->
+      <div class="d-flex align-items-center" v-if="roleStore.types.length">
+        <label for="type-selector" class="me-2 mb-0">Выбор типа:</label>
+        <select
+          class="form-select form-select-sm w-auto"
+          id="type-selector"
+          v-model="roleStore.selectedTypes"
+          @change="onTypeChange"
+        >
+          <option
+            v-for="(type, key) in roleStore.types"
+            :key="key"
+            :value="type"
+          >
+            {{ type }}
+          </option>
+        </select>
+      </div>
     </div>
   </header>
 </template>
 
 <script>
 import { useRoleStore } from '../store/index.js'
+import BackButton from '@/modules/shared/components/BackButton.vue'
 
 export default {
+  components: { BackButton },
   setup() {
     const roleStore = useRoleStore()
 
+    // Загружаем данные из localStorage при инициализации
     const loadSelectedRoleFromStorage = () => {
       const storedRole = localStorage.getItem('selectedRole')
       const storedType = localStorage.getItem('selectedTypes')
@@ -65,22 +68,34 @@ export default {
       }
     }
 
+    // Обработчик смены роли
     const onRoleChange = () => {
       console.log('Выбранная роль:', roleStore.selectedRole)
       localStorage.setItem('selectedRole', roleStore.selectedRole)
     }
 
+    // Обработчик смены типа
     const onTypeChange = () => {
       console.log('Выбранный тип:', roleStore.selectedTypes)
       localStorage.setItem('selectedTypes', roleStore.selectedTypes)
     }
 
+    // Обработчик очистки данных
+    const clearStorage = () => {
+      localStorage.removeItem('selectedRole')
+      localStorage.removeItem('selectedTypes')
+      roleStore.setSelectedRole(null)
+      roleStore.selectedTypes = null
+    }
+
+    // Инициализация
     loadSelectedRoleFromStorage()
 
     return {
       roleStore,
       onRoleChange,
       onTypeChange,
+      clearStorage,
     }
   },
 }
