becf1be6cfa3c6a088aaed6d6216c97e51a1d416
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-11-27 09:56:20 +0300

    +

--- client/src/modules/orders/form-1-orders/components/PagesOrders.vue
+++ client/src/modules/orders/form-1-orders/components/PagesOrders.vue
@@ -31,7 +31,7 @@
 import { computed, onMounted, ref, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import _ from 'lodash'
-import { getOrders } from '../api/list.ts'
+import { getOrders } from '../api/list.js'
 import ServerSideTable from '@/modules/shared/tables/table-server/ServerSideTable.vue'
 import { useRoleStore } from '@/modules/_main/store/index.js'
 import { statuses } from '@/modules/shared/logic/statuses.js'
