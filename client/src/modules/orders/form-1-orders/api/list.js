23e5ce650dbab78e8ee5cae2261a7aaf69ecc239
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-11-27 09:56:03 +0300

    refactor(api): пересадка list.ts с TypeScript на JavaScript
    - Файл list.ts был переписан на JavaScript.
    - Удалены типы TypeScript, добавлены проверки типов через JSDoc.
    - Изменения не затрагивают функционал, но упрощают поддержку файлов без использования TypeScript.

---
+++ client/src/modules/orders/form-1-orders/api/list.js
@@ -0,0 +1,23 @@
+import apiClient from '@/modules/api/apiClient'
+import { handleResponse, handleError } from '@/modules/api/responseHandlers'
+
+/**
+ * Получает список заказов с использованием API.
+ *
+ * @param {Object} params - Параметры запроса.
+ * @returns {Promise<Array>} - Промис с массивом заказов.
+ */
+export const getOrders = async (params) => {
+  try {
+    const now = new Date()
+    const formattedTime = `${now.getFullYear().toString().slice(-2)}:${String(
+      now.getMonth() + 1
+    ).padStart(2, '0')}:${String(now.getSeconds()).padStart(2, '0')}`
+    console.log(`[${formattedTime}] Request params:`, params)
+
+    const response = await apiClient.get('list', { params })
+    return handleResponse(response)
+  } catch (error) {
+    return handleError(error)
+  }
+}
