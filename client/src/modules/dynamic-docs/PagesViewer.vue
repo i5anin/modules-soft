6f00691400c7d6f98c74b3ebb4d51b2a78319fd8
Isanin Sergey <sergey.isanin@gmail.com>
2024-12-01 20:35:08 +0300

    refactor(ui): разделение InfoComponent на микрокомпоненты и рефакторинг
    - Разделил `InfoComponent.vue` на несколько микрокомпонентов для улучшения структуры кода:
      - `ChildrenList.vue`
      - `CollapsibleSection.vue`
      - `EmitsList.vue`
      - `MethodsList.vue`
      - `PropsList.vue`
    - Обновил `DynamicComponentViewer.vue` и `PagesViewer.vue` для использования новых компонентов.
    - Улучшил читаемость и поддерживаемость кода, следуя принципам из книги "Чистый код" Роберта Мартина.
    - Логика получения данных оставлена в тех же файлах, где данные используются, для повышения прозрачности.

--- client/src/modules/dynamic-docs/PagesViewer.vue
+++ client/src/modules/dynamic-docs/PagesViewer.vue
@@ -4,18 +4,7 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import PagesClients from '@/modules/clients-noms/form-2-noms/components/PagesClientNoms.vue';
 import InfoComponent from './InfoComponent.vue';
-
-export default {
-  components: {
-    InfoComponent,
-  },
-  setup() {
-    return {
-      PagesClients, // Импортируем описание компонента
-    };
-  },
-};
 </script>
