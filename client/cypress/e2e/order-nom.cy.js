691492c5c1d8a1410be1995b3d49c99f3ef0878a
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-12-10 10:45:23 +0300

    Revert "Merge branch 'minor-updates' of https://github.com/i5anin/modules-soft__pf-forum into minor-updates"
    This reverts commit 213b009d6125d751727f2b5fbad763d66a5fdf97, reversing
    changes made to 80cdb6a84a8e4bd67beedc30a9322000c8b30ba8.

--- client/cypress/e2e/order-nom.cy.js
+++ client/cypress/e2e/order-nom.cy.js
@@ -1,30 +1,38 @@
-describe('Страница номенклатуры ордера с разными ролями', () => {
-  const orderId = 1840
-  const orderUrl = `/order/${orderId}/noms`
-  const roles = ['managers', 'metrolog', 'omts', 'tech_calc']
+describe('Страница номенклатуры order с разными ролями', () => {
+  const orderId = 1840 // Идентификатор ордера для формирования URL страницы
+  const orderUrl = `/order/${orderId}/noms` // URL страницы номенклатуры
+  const roles = ['managers', 'metrolog', 'omts', 'tech_calc'] // Список ролей для проверки
 
   roles.forEach((role) => {
     it(`должна корректно загружаться для роли "${role}"`, () => {
-      // Устанавливаем значение в localStorage
+      // Устанавливаем выбранную роль в localStorage перед загрузкой страницы
       cy.visit(orderUrl, {
         onBeforeLoad(win) {
           win.localStorage.setItem('selectedRole', role)
         },
       })
 
-      // Проверяем, что страница загрузилась
+      // Проверяем, что URL страницы содержит идентификатор ордера и корректный путь
       cy.url().should('include', orderUrl)
 
-      // Проверяем, что значение из localStorage установлено правильно
+      // Убеждаемся, что значение роли правильно установлено в localStorage
       cy.window().then((win) => {
         expect(win.localStorage.getItem('selectedRole')).to.eq(role)
       })
 
-      // Проверяем, что таблица с данными отображается
-      cy.get('.table-sbor').should('exist').and('be.visible')
+      // Небольшая задержка перед проверкой таблицы
+      cy.wait(100)
+
+      // Проверяем, что таблица с данными отображается на странице
+      cy.get('.table-sbor')
+        .should('exist') // Убедимся, что таблица существует в DOM
+        .and('be.visible') // Убедимся, что таблица видима пользователю
+
+      // Проверяем, что таблица содержит хотя бы одну строку с данными
       cy.get('.table-sbor tbody tr').should('have.length.greaterThan', 0)
 
-      // Дополнительные проверки для каждого значения роли можно добавить здесь
+      // Здесь можно добавить дополнительные проверки для специфических ролей
+      // Например, проверка доступных кнопок или других элементов интерфейса
     })
   })
 })
