f345bbee753ed9bbffa8281581193bba5d77ac40
Isanin Sergey pf-forum <92434473+i5anin@users.noreply.github.com>
2024-12-18 16:52:12 +0300

    refactor(ui): переписан компонент SborNode на Composition API
    - Переведён компонент SborNode на Composition API;
    - Оптимизирована структура кода для повышения читаемости;
    - Улучшена обработка событий и вычислений;
    - Сохранена логика рекурсивного вызова дочерних узлов;
    - Переписаны стили, обеспечив соответствие современным стандартам.
    
    Изменения улучшили поддержку и читаемость компонента, сохранив его функциональность.

--- client/src/modules/shared/tables/sborka/SborNode.vue
+++ client/src/modules/shared/tables/sborka/SborNode.vue
@@ -43,9 +43,8 @@
           class="branch-line"
           :class="{ 'last-child': isLastChild }"
           :style="{ left: `${(depth - 1) * 20}px` }"
-        ></div>
-
-        <div class="node-content">
+        />
+        <div class="node-content" @click.stop="handleRowClick">
           <font-awesome-icon
             :icon="sbor.is_sbor ? ['fas', 'cubes'] : ['fas', 'cube']"
             :style="{ color: sbor.is_sbor ? '#dc6611' : '#cfa614' }"
@@ -53,12 +52,6 @@
           />
           <span
             v-html="formatValue(sbor[field.name], field.type, field.name)"
-          ></span>
-          <font-awesome-icon
-            :icon="['fas', 'circle-info']"
-            :style="{ color: 'green' }"
-            class="icon-sm ms-2 me-2"
-            @click.stop="handleRowClick"
           />
         </div>
       </div>
@@ -74,7 +67,7 @@
         v-html="formatValue(sbor[field.name], field.type, field.name)"
         :title="generateTitle(field)"
         style="font-size: 13px"
-      ></span>
+      />
     </td>
   </tr>
 
@@ -92,7 +85,7 @@
   </template>
 </template>
 
-<script>
+<script setup>
 import { computed, ref, toRefs, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import { store } from './store.js'
@@ -102,86 +95,79 @@ import StatusDisplay from '@/modules/shared/components/StatusDisplay.vue'
 import StrategyDisplay from '@/modules/shared/components/StrategyDisplay.vue'
 import './SborNode.css'
 
-export default {
-  name: 'SborNode',
-  components: { FontAwesomeIcon, StatusDisplay, StrategyDisplay },
-  props: {
-    sbor: { type: Object, required: true },
-    depth: { type: Number, default: 0 },
-    fields: { type: Array, default: () => [] },
-    isLastChild: { type: Boolean, default: false },
-    detail: {
-      type: Object,
-      required: true,
-      default: () => ({ route: '', idKey: '' }),
-    },
+const props = defineProps({
+  sbor: { type: Object, required: true },
+  depth: { type: Number, default: 0 },
+  fields: { type: Array, default: () => [] },
+  isLastChild: { type: Boolean, default: false },
+  detail: {
+    type: Object,
+    required: true,
+    default: () => ({ route: '', idKey: '' }),
   },
-  setup(props) {
-    const { sbor, detail } = toRefs(props)
-    const sborStore = store()
-    const router = useRouter()
-    const isExpanded = ref(false)
+})
 
-    watch(
-      detail,
-      (newDetail) => {
-        if (!newDetail.route || !newDetail.idKey) {
-          console.warn(
-            'Detail prop is missing or incomplete. Expected { route: "", idKey: "" }.',
-            newDetail
-          )
-        }
-      },
-      { immediate: true }
-    )
+const { sbor, detail } = toRefs(props)
+const sborStore = store()
+const router = useRouter()
+const isExpanded = ref(false)
 
-    const toggle = () => {
-      if (hasChildren.value) {
-        isExpanded.value = !isExpanded.value
-      }
-    }
-
-    const handleRowClick = () => {
-      const idKey = detail.value.idKey
-      const id = sbor.value[idKey]
-
-      if (!id) {
-        console.error('Missing required parameter: id', {
-          id,
-          sbor: sbor.value,
-        })
-        return
-      }
-
-      router
-        .push({
-          name: detail.value.route,
-          params: { [idKey]: id, nom_id: id },
-        })
-        .catch((error) => {
-          console.error('Error navigating to route:', error)
-        })
-    }
-
-    const hasChildren = computed(
-      () => sbor.value.sbor_tree && sbor.value.sbor_tree.length > 0
-    )
-
-    const fieldsArray = computed(() => sborStore.filteredFields)
-
-    const generateTitle = (field) =>
-      `Поле: ${field.title || 'Нет данных'}\nПеременная: ${field.name || 'Нет данных'}`
-
-    return {
-      isExpanded,
-      toggle,
-      handleRowClick,
-      fieldsArray,
-      hasChildren,
-      formatValue,
-      generateTitle,
-      getTextAlignment,
+watch(
+  detail,
+  (newDetail) => {
+    if (!newDetail.route || !newDetail.idKey) {
+      console.warn(
+        'Detail prop is missing or incomplete. Expected { route: "", idKey: "" }.',
+        newDetail
+      )
     }
   },
+  { immediate: true }
+)
+
+const toggle = () => {
+  if (hasChildren.value) {
+    isExpanded.value = !isExpanded.value
+  }
 }
+
+const handleRowClick = () => {
+  const idKey = detail.value.idKey
+  const id = sbor.value[idKey]
+
+  if (!id) {
+    console.error('Missing required parameter: id', {
+      id,
+      sbor: sbor.value,
+    })
+    return
+  }
+
+  router
+    .push({
+      name: detail.value.route,
+      params: { [idKey]: id, nom_id: id },
+    })
+    .catch((error) => {
+      console.error('Error navigating to route:', error)
+    })
+}
+
+const hasChildren = computed(
+  () => sbor.value.sbor_tree && sbor.value.sbor_tree.length > 0
+)
+
+const fieldsArray = computed(() => sborStore.filteredFields)
+
+const generateTitle = (field) =>
+  `Поле: ${field.title || 'Нет данных'}\nПеременная: ${field.name || 'Нет данных'}`
 </script>
+
+<style scoped>
+.table-info {
+  background-color: #f8f9fa;
+}
+.fw-bold {
+  font-weight: bold;
+}
+</style>
