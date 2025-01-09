a6cee1822f7345afde714b140ac1975789fbc5c9
Isanin Sergey pf-forum <92434473+i5anin@users.noreply.github.com>
2025-01-09 14:13:30 +0300

    build(vite): добавлены плагины vue-devtools и html
    build(vite): добавлены плагины vue-devtools и html
    
    - Добавлен плагин `vite-plugin-vue-devtools` для улучшения отладки;
    - Подключен `vite-plugin-html` для работы с HTML-файлами;
    - Обновлена конфигурация `vite.config.js` с поддержкой новых плагинов.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { createHtmlPlugin } from 'vite-plugin-html'
 import path from 'path' // Импортируем модуль path
 
 export default defineConfig({
   server: { host: true, port: 5173 },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueDevTools(), // Добавляем плагин Vue DevTools
+    createHtmlPlugin({}), // Добавляем плагин HTML
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), //  указываем путь к папке src
+      '@': path.resolve(__dirname, './src'), // Указываем путь к папке src
     },
   },
 })
