95aafb3e648c0e62f5fa2205792bab2d086e216d
PizZzA <sergey.isanin@gmail.com>
2025-03-16 17:48:35 +0300

    +

--- doc/projectConfig.js
+++ doc/projectConfig.js
@@ -1,5 +1,9 @@
 // 📌 Основная конфигурация проекта
 
-export const PROJECT_NAME = "Система выдачи инструмента на производстве";
-export const PROJECT_DESCRIPTION = "Автоматизированная система учета и выдачи инструмента.";
-export const WAKATIME_BADGE = "[![wakatime](https://wakatime.com/badge/github/i5anin/tools-soft__pf-forum.svg)](https://wakatime.com/badge/github/i5anin/tools-soft__pf-forum)";
+export const PROJECT_NAME = "Модульный фронтенд универсальных таблиц";
+export const PROJECT_DESCRIPTION =
+    "Гибкая система для работы с таблицами: динамическое построение, " +
+    "иерархическая структура в сборках, адаптивные компоненты.";
+
+export const WAKATIME_BADGE =
+    "[![wakatime](https://wakatime.com/badge/github/i5anin/modules-soft__pf-forum.svg)](https://wakatime.com/badge/github/i5anin/modules-soft__pf-forum)" 
