6b05bd6ede0c24abf936983f2ea3739da95ea8bd
Isanin Sergey <sergey.isanin+github@gmail.com>
2025-03-26 15:41:00 +0300

    refactor(store): удалена логика ролей из useRoleStore
    📌 Удалены:
    - `selectedRole`, `roleNames`, `selectedTypes`, `types`;
    - метод `setSelectedRole`;
    - геттер `roleDisplayName`.

--- src/modules/_main/store/store.js
+++ src/modules/_main/store/store.js
@@ -2,28 +2,7 @@
 import { defineStore } from 'pinia'
 
 export const useRoleStore = defineStore('roleStore', {
-  state: () => ({
-    selectedRole: 'metrolog',
-    roleNames: {
-      managers: 'Менеджер',
-      metrolog: 'Метролог',
-      omts: 'ОМТС',
-      tech_calc: 'Технолог',
-    },
-    selectedTypes: 'orders',
-    types: {
-      orders: 'orders',
-      specs: 'specs',
-    },
-  }),
-  actions: {
-    setSelectedRole(role) {
-      this.selectedRole = role
-    },
-  },
-  getters: {
-    roleDisplayName(state) {
-      return state.roleNames[state.selectedRole]
-    },
-  },
+  state: () => ({}),
+  actions: {},
+  getters: {},
 })
